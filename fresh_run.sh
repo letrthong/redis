6d395a1ab89aae15b7a6941ba2b3184e8c355551
Thong LT <letrthong@gmail.com>
2025-05-05 10:46:56 +0700

    Update fresh_run.sh

--- fresh_run.sh
+++ fresh_run.sh
@@ -1,8 +1,16 @@
 # https://docs.docker.com/engine/install/debian/
 # git pull origin master
 # git submodule update --init
-#stop all containers
+
+# free space
+docker system prune -a
+
+# stop all containers
 docker stop $(docker ps -a -q)
+
+# Removing all containers
+docker container prune
+
 #remove all containers
 docker rm $(docker ps -a -q)
 #remove all unused images to free disk
