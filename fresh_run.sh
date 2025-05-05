0d6e6f9df2da3a8430c20caacaea1f500b82b579
Thong LT <letrthong@gmail.com>
2025-05-05 10:47:36 +0700

    Update fresh_run.sh

--- fresh_run.sh
+++ fresh_run.sh
@@ -1,6 +1,5 @@
 # https://docs.docker.com/engine/install/debian/
-# git pull origin master
-# git submodule update --init
+# https://depot.dev/blog/docker-clear-cache
 
 # free space
 docker system prune -a
@@ -13,11 +12,15 @@ docker container prune
 
 #remove all containers
 docker rm $(docker ps -a -q)
+
 #remove all unused images to free disk
 docker image prune -a -f
+
 #remove the rest containers if have
 docker compose down
+
 # build images
 docker compose build --no-cache
+
 # start up containers from all images
 docker compose up
